1f882723507a8b949b1e96a35c1847f742189e70
Alen Šiljak <dev@alensiljak.eu.org>
2022-12-18 14:53:55 +0100

    add Svelte Material UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script lang="ts">
+	import Button, { Label } from '@smui/button';
+
+	let x = 10;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p>X is {x}</p>
+
+<Button>
+    <Label>Yo!</Label>
+</Button>
