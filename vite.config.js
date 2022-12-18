508c186f89bd6f46f16b4737c0f06142cb086de9
Alen Šiljak <dev@alensiljak.eu.org>
2022-12-18 13:51:31 +0100

    SPA setting

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit()
+		// SvelteKitPWA({ /* pwa options */ })
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
