e6c26a019a42b11e4703abcc50939fddf1125e70
Alen Šiljak <dev@alensiljak.eu.org>
2022-12-18 14:21:16 +0100

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+// import { VitePWA } from 'vite-plugin-pwa'
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [
-		sveltekit()
-		// SvelteKitPWA({ /* pwa options */ })
+		sveltekit(),
+		// SvelteKitPWA({ /* pwa options */ }),
+		// VitePWA({ registerType: 'prompt'})
 	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
