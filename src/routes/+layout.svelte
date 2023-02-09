da485cf9b81d9fc21782a67ea9d3c75a631e74a5
Alen Šiljak <dev@alensiljak.eu.org>
2023-02-09 17:49:33 +0100

    docs for pwa are messed up

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,42 @@
+<script lang="ts">
+	import Drawer, { Header, Title, Subtitle } from '@smui/drawer';
+	// import { drawerVisible } from '../store/store';
+	import { onMount } from 'svelte';
+	import { pwaInfo } from 'virtual:pwa-info';
+
+	// let is_drawer_open = true; // drawerVisible
+
+	// drawerVisible.subscribe((value) => is_drawer_open = value);
+
+    let ReloadPrompt: any
+	onMount(async () => {
+        pwaInfo && (ReloadPrompt = (await import('$lib/ReloadPrompt.svelte')).default)
+	});
+
+	$: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : ''
+</script>
+
+<svelte:head>
+	{@html webManifest}
+</svelte:head>
+
 <p>This is the layout</p>
 
 <!-- Drawer / Sidebark -->
 <nav class="drawer-container">
-    <!-- bind:is_drawer_open -->
-    <Drawer variant="dismissible">
-        <Header>
-            <Title>Super Drawer</Title>
-            <Subtitle>It's the best drawer.</Subtitle>
-          </Header>
-    </Drawer>
+	<!-- bind:is_drawer_open -->
+	<Drawer variant="dismissible">
+		<Header>
+			<Title>Super Drawer</Title>
+			<Subtitle>It's the best drawer.</Subtitle>
+		</Header>
+	</Drawer>
 </nav>
 
-<slot></slot>
+<main>
+<slot />
+</main>
 
-<script lang="ts">
-    import Drawer, { Header, Title, Subtitle } from '@smui/drawer';
-    import { drawerVisible } from '../store/store';
-    
-    let is_drawer_open = true; // drawerVisible
-
-    drawerVisible.subscribe((value) => is_drawer_open = value);
-
-</script>
+{#if ReloadPrompt}
+  <svelte:component this={ReloadPrompt} />
+{/if}
