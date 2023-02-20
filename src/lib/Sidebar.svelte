c36583615d2e7f07a42303989e92c27c67b79432
Alen Šiljak <dev@alensiljak.eu.org>
2023-02-20 15:25:06 +0100

    swipe-able sidebar

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -4,6 +4,7 @@
 	import Drawer, { Header, Title, Subtitle } from '@smui/drawer';
 	import { drawerVisible } from '$lib/store';
 	import { onDestroy } from 'svelte';
+	import Scrim from '@smui/drawer/src/Scrim.svelte';
 
 	let open = $drawerVisible;
 
@@ -12,10 +13,13 @@
 	});
 	onDestroy(unsubscribe);
 </script>
-
-<Drawer variant="dismissible" bind:open>
+<!-- variant="dismissible" -->
+<Drawer variant="modal" fixed={false} bind:open>
 	<Header>
 		<Title>Super Drawer</Title>
 		<Subtitle>It's the best drawer.</Subtitle>
+		
+		<button>?</button>
 	</Header>
 </Drawer>
+<Scrim fixed={false} />
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,8 @@
 	import { onMount } from 'svelte';
 	import { pwaInfo } from 'virtual:pwa-info';
 	import { AppContent } from '@smui/drawer';
+	import { swipe } from 'svelte-gestures';
 	import { drawerVisible } from '$lib/store';
-
-	// let is_drawer_open = true; // drawerVisible
-
 
 	/// PWA reload
 	let ReloadPrompt: any;
@@ -19,21 +17,59 @@
 	$: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : '';
 	///
 
+	// Swipe
+	let direction: string;
+
+	function handler(event: any) {
+		direction = event.detail.direction;
+	}
+
+	function onSwipe(event: any) {
+		// console.log('swiping');
+		direction = event.detail.direction;
+
+		if (direction == 'left') {
+			drawerVisible.set(false);
+		} else if (direction === 'right') {
+			drawerVisible.set(true);
+		}
+	}
 </script>
 
 <svelte:head>
 	{@html webManifest}
 </svelte:head>
+<svelte:body
+	use:swipe={{ timeframe: 300, minSwipeDistance: 80, touchAction: 'pan-y' }}
+	on:swipe={onSwipe}
+/>
 
 <Sidebar />
 <AppContent>
 	<TopBar />
 	<main>
 		<slot />
+
+		<div>
+			This one <b>swipes only in horizontal directions</b>:
+			<span style="color:red;">{direction}</span><br />
+			You can scroll vertically as normal. It is due to <b>touchAction: 'pan-y'</b>
+		</div>
 	</main>
-	
 </AppContent>
+<!-- </div> -->
 
 {#if ReloadPrompt}
 	<svelte:component this={ReloadPrompt} />
 {/if}
+
+<style lang="scss">
+	:global(body) {
+		height: 97vh;
+		//min-height: 100%;
+		// background-color: red;
+	}
+	:global(body.dark-mode) {
+		background-color: rebeccapurple;
+	}
+</style>
