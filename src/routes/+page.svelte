be2f5b561f223fdb560b58a940a04716c10f7185
Alen Šiljak <462445+alensiljak@users.noreply.github.com>
2023-10-18 17:20:54 +0200

    adding framework7 route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div>
+    Sub pages:
+
+    <ul>
+        <li><a href="framework7">Framework7</a></li>
+    </ul>
+</div>
