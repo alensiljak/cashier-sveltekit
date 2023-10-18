391c10b8214ad44017bcc5c3aaec68826bf339ab
Alen Šiljak <462445+alensiljak@users.noreply.github.com>
2023-10-18 19:53:39 +0200

    new route

--- src/routes/framework7/+page.svelte
+++ src/routes/framework7/+page.svelte
@@ -1,6 +1,17 @@
 <p>Let's see if Framework7 works</p>
 
+<div class="page" data-name="home">
+    <div class="page-content">
+      ... scrollable page content goes here ...
+    </div>
+  </div>
+
 <script>
-import Framework7 from 'framework7';
+// import Framework7, {getDevice} from 'framework7';
+import Framework7, {getDevice} from 'framework7/lite';
+
+// var app = new Framework7({/*...*/});
+var device = getDevice();
+console.log('device:', device);
 
 </script>
