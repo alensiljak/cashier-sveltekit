f3250b13453979efc6720f51029100276d5413f4
Alen Šiljak <462445+alensiljak@users.noreply.github.com>
2024-11-14 19:17:11 +0100

    drawer sample

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,17 @@
 <script>
-	import '../app.css';
+import { initializeStores, Drawer } from '@skeletonlabs/skeleton';
+import { getDrawerStore } from "@skeletonlabs/skeleton";
+import '../app.css';
+
+initializeStores();
+const drawerStore = getDrawerStore();
+
+drawerStore.open();
+// drawerStore.close();
 </script>
+
+<Drawer width="w-[300px]">
+	Cashier
+</Drawer>
 
 <slot></slot>
