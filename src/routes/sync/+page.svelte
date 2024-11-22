59d0e304b76cf535fe1733e5d3cfe99c5ab9fb19
Alen Šiljak <462445+alensiljak@users.noreply.github.com>
2024-11-22 16:05:00 +0100

    loading settings, button animation

--- src/routes/sync/+page.svelte
+++ src/routes/sync/+page.svelte
@@ -1,13 +1,34 @@
-<script>
-	import Toolbar from '$lib/components/toolbar.svelte';
-	import { RefreshCcw } from 'lucide-svelte';
-	import { onMount } from 'svelte';
+<script lang="ts">
+	import Toolbar from '$lib/components/toolbar.svelte'
+	import { RefreshCcw } from 'lucide-svelte'
+	import { onMount } from 'svelte'
+    import {SettingKeys, settings} from '$lib/settings'
 
-	let serverUrl = 'http://localhost:3000';
+	let serverUrl = 'http://localhost:3000'
+    let syncAccounts = false
+    let syncAaValues = false
+    let syncPayees = false
+
+    let rotationClass = ''
 
 	onMount(async () => {
-		// todo: load sync settings
+		// load sync settings
+        await loadSettings()
 	});
+
+    async function loadSettings() {
+        serverUrl = await settings.get(SettingKeys.syncServerUrl)
+
+        syncAccounts = await settings.get(SettingKeys.syncAccounts)
+        syncAaValues = await settings.get(SettingKeys.syncAaValues)
+        syncPayees = await settings.get(SettingKeys.syncPayees)
+    }
+
+    function onSyncClicked() {
+        rotationClass = rotationClass == ''
+            ? 'animate-[spin_2s_linear_infinite]'
+            : ''
+    }
 </script>
 
 <Toolbar title="Cashier Sync" />
@@ -21,25 +42,30 @@
 		<input class="input" type="text" placeholder="Server URL" bind:value={serverUrl} />
 	</label>
 
+	<center>
+		<h3 class="h3">Synchronize</h3>
+	</center>
+
 	<div class="flex flex-col space-y-8 pt-6">
 		<label class="flex items-center space-x-2">
-			<input class="checkbox" type="checkbox" />
+			<input class="checkbox" type="checkbox" bind:checked={syncAccounts} />
 			<p>Sync account list with balances</p>
 		</label>
 		<label class="flex items-center space-x-2">
-			<input class="checkbox" type="checkbox" />
+			<input class="checkbox" type="checkbox" bind:checked={syncAaValues} />
 			<p>Sync account balances in base currency, for asset allocation.</p>
 		</label>
 		<label class="flex items-center space-x-2">
-			<input class="checkbox" type="checkbox" />
+			<input class="checkbox" type="checkbox" bind:checked={syncPayees} />
 			<p>Sync Payees</p>
 		</label>
 	</div>
 
-    <center class="pt-10">
-        <button class="btn bg-tertiary-500 uppercase text-secondary-500">
-            <span><RefreshCcw /></span>
-            <span>Sync</span>
-        </button>
-    </center>
+	<center class="pt-10">
+		<button class="btn bg-tertiary-500 uppercase text-secondary-500"
+            onclick={onSyncClicked}>
+			<span><RefreshCcw class="{rotationClass}" style="animation-direction: reverse;" /></span>
+			<span>Sync</span>
+		</button>
+	</center>
 </main>
