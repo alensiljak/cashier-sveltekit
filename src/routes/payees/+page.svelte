40b7c215be858fa4c563b2df34c8b93730f8eb38
Alen Šiljak <462445+alensiljak@users.noreply.github.com>
2024-11-23 12:51:09 +0100

    payees page in 10s

--- src/routes/payees/+page.svelte
+++ src/routes/payees/+page.svelte
@@ -1,1 +1,29 @@
-<h1>Payees</h1>
+<script lang="ts">
+	import Toolbar from '$lib/components/Toolbar.svelte';
+	import CashierDAL from '$lib/data/dal';
+	import type { Payee } from '$lib/data/model';
+	import { onMount } from 'svelte';
+
+	let payees: Array<Payee> = [];
+
+	onMount(async () => {
+		await loadData();
+	});
+
+	async function loadData() {
+		const dal = new CashierDAL();
+		payees = await dal.loadPayees().toArray();
+	}
+</script>
+
+<main class="flex h-screen flex-col">
+	<Toolbar title="Payees" />
+	<!-- search toolbar -->
+	<div class={`h-screen overflow-auto`}>
+		{#each payees as payee}
+			<div>
+				{payee.name}
+			</div>
+		{/each}
+	</div>
+</main>
