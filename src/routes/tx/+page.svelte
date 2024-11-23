944299e8eac3fa15eeef79c6cc9fc2e169c04685
Alen Šiljak <462445+alensiljak@users.noreply.github.com>
2024-11-23 11:40:47 +0100

    saving and navigation

--- src/routes/tx/+page.svelte
+++ src/routes/tx/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import Fab from '$lib/components/FAB.svelte';
 	import Toolbar from '$lib/components/Toolbar.svelte';
 	import { Check } from 'lucide-svelte';
@@ -6,26 +6,65 @@
 	import { onMount } from 'svelte';
 	import { get } from 'svelte/store';
 	import ToolbarMenuItem from '$lib/components/ToolbarMenuItem.svelte';
+	import { afterNavigate, goto } from '$app/navigation';
+	import Notifier from '$lib/utils/notifier';
+	import CashierDAL from '$lib/data/dal';
+	import type { Transaction } from '$lib/data/model';
+	import { SettingKeys, settings } from '$lib/settings';
+	import appService from '$lib/services/appService';
+	import { base } from '$app/paths';
 
-    onMount(() => {
-        // Get the Xact from the store?
-        // No, it is available already.
-        console.log(get(xact))
-    })
+	let previousPage: string = base;
 
-	function onFab() {
-		console.log('fab clicked!');
+	onMount(() => {
+		// Get the Xact from the store?
+		// No, it is available already.
+		if (!get(xact)) {
+			goto('/');
+		}
+	});
+
+	afterNavigate(({ from }) => {
+		previousPage = from?.url.pathname || previousPage;
+	});
+
+	async function onFab() {
+		// console.log('fab clicked!');
+		try {
+			await saveXact();
+		} catch (e: any) {
+			Notifier.error(e.message);
+		}
+	}
+
+	/**
+	 * save transaction
+	 */
+	async function saveXact() {
+		const tx = get(xact) as Transaction;
+		const dal = new CashierDAL();
+
+		await dal.saveTransaction(tx);
+
+		// Remember Last Transaction?
+		const remember = await settings.get(SettingKeys.rememberLastTransaction);
+		if (remember) {
+			await appService.saveLastTransaction(tx);
+		}
+
+		//goto(previousPage);
+		history.back();
 	}
 </script>
 
 <Toolbar title="Journal Entry">
-    {#snippet menuItems()}
-        <ToolbarMenuItem text="Save" />
-        <ToolbarMenuItem text="Reset" />
-        {/snippet}
+	{#snippet menuItems()}
+		<ToolbarMenuItem text="Save" />
+		<ToolbarMenuItem text="Reset" />
+	{/snippet}
 </Toolbar>
 <Fab Icon={Check} onclick={onFab} />
 
 <!-- tx editor -->
 
-<!-- confirm deletion dialog -->
+<!-- dialog for confirming reset -->
