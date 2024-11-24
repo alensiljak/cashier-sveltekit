7ae465c543427f0e81023fe8774d5053cbfe4442
Alen Šiljak <462445+alensiljak@users.noreply.github.com>
2024-11-24 19:51:03 +0100

    left-align toolbar text

--- src/lib/components/toolbar.svelte
+++ src/lib/components/toolbar.svelte
@@ -36,12 +36,13 @@
 >
 	{#snippet lead()}
 		<!-- <ArrowLeft size={24} /> -->
-		<button class="btn btn-sm py-1 pl-2 lg:hidden" onclick={toggleSidebar}>
+		<button class="btn btn-sm py-1 pl-2 pr-0 lg:hidden" onclick={toggleSidebar}>
 			<span>
 				<Menu size={24} />
 			</span>
 		</button>
 		<!-- <strong class="text-xl uppercase">Skeleton</strong> -->
+	<h4 class="h4 leading-9 pl-0">{title}</h4>
 	{/snippet}
 	<!-- {#snippet children()}
 	{/snippet} -->
@@ -63,5 +64,5 @@
 			<h2 class="h2">Headline</h2>
 		{/snippet} -->
 
-	<h4 class="h4 leading-9">{title}</h4>
+	<!-- <h4 class="h4 leading-9">{title}</h4> -->
 </AppBar>
