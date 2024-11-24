a21481eb862e62abef1104a27adf9550ab474c1b
Alen Šiljak <462445+alensiljak@users.noreply.github.com>
2024-11-24 20:07:11 +0100

    styling the search bar

--- src/routes/payees/+page.svelte
+++ src/routes/payees/+page.svelte
@@ -5,15 +5,19 @@
 	import { onMount } from 'svelte';
 	import { selectionMetadata } from '$lib/data/mainStore';
 	import Fab from '$lib/components/FAB.svelte';
-	import { CheckIcon } from 'lucide-svelte';
+	import { CheckIcon, SearchIcon } from 'lucide-svelte';
 
 	let payees: Array<Payee> = [];
-		let isInSelectionMode = false;
+	let isInSelectionMode = false;
+	let searchField: HTMLInputElement;
 
 	onMount(async () => {
 		await loadData();
 
 		isInSelectionMode = $selectionMetadata !== undefined;
+
+		// focus on filter
+		searchField.focus()
 	});
 
 	async function loadData() {
@@ -25,23 +29,22 @@
 	 * Accept the search term as the Payee.
 	 */
 	function onFabClick() {
-        // if(string.IsNullOrWhiteSpace(searchTerm))
-        // {
-        //     Notification.Warning("The search term is empty!");
-        //     return;
-        // }
-
-        // await onPayeeSelected(searchTerm);
+		// if(string.IsNullOrWhiteSpace(searchTerm))
+		// {
+		//     Notification.Warning("The search term is empty!");
+		//     return;
+		// }
+		// await onPayeeSelected(searchTerm);
 	}
 
 	function onPayeeSelected(name: string) {
 		if (isInSelectionMode) {
 			if ($selectionMetadata) {
-				if($selectionMetadata?.selectionType !== 'payee') {
-				throw('Invalid selection mode')
-			}
+				if ($selectionMetadata?.selectionType !== 'payee') {
+					throw 'Invalid selection mode';
+				}
 
-			// store the selection.
+				// store the selection.
 				$selectionMetadata.selectedId = name;
 			}
 
@@ -50,7 +53,6 @@
 			// goto('/account') // show account details
 			console.info('ignore');
 		}
-
 	}
 </script>
 
@@ -59,7 +61,16 @@
 <main class="flex h-screen flex-col">
 	<Toolbar title="Payees" />
 	<!-- search toolbar -->
-	<div class={`h-screen overflow-auto`}>
+	<div class="bg-primary-500">
+		<div class="input-group input-group-divider mx-auto w-5/6 grid-cols-[1fr_auto] lg:w-2/5 rounded-full">
+			<input type="search" placeholder="Search..." class="" bind:this={searchField} />
+			<div class="text-white"><SearchIcon /></div>
+		</div>
+		<!-- <button class="variant-filled-secondary">Submit</button> -->
+		<!-- <div class="input-group-shim"><SearchIcon /></div> -->
+	</div>
+
+	<div class="h-screen overflow-auto">
 		{#each payees as payee}
 			<!-- svelte-ignore a11y_click_events_have_key_events -->
 			<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
@@ -68,7 +79,7 @@
 				onclick={() => onPayeeSelected(payee.name)}
 				role="listitem"
 			>
-			{payee.name}
+				{payee.name}
 			</div>
 		{/each}
 	</div>
