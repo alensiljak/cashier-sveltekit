5713ecfb95982da90f6cf21a4a13b492541e587b
Alen Šiljak <462445+alensiljak@users.noreply.github.com>
2024-11-25 09:26:31 +0100

    beginning work on Journal

--- src/routes/journal/+page.svelte
+++ src/routes/journal/+page.svelte
@@ -1,1 +1,27 @@
-<h1>Journal</h1>
+<script lang="ts">
+	import Toolbar from '$lib/components/Toolbar.svelte';
+	import type { Xact } from '$lib/data/model';
+	import { onMount } from 'svelte';
+
+    let xacts: Xact[] = $state([])
+
+    onMount(async () => {
+        // load data
+    })
+</script>
+
+<Toolbar title="Journal">
+	<!-- Export -->
+	<!-- Delete All -->
+</Toolbar>
+
+<main>
+    {#if xacts.length == 0}
+    <p>The device journal is empty</p>
+    {:else}
+    {#each xacts as xact}
+        <span>{xact.payee}</span>
+
+    {/each}
+    {/if}
+</main>
