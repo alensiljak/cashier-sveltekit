76fe96b1942b36d63503869ba511c22a0d1bb276
Alen Šiljak <462445+alensiljak@users.noreply.github.com>
2024-11-25 15:26:28 +0100

    load xacts

--- src/routes/journal/+page.svelte
+++ src/routes/journal/+page.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
 	import Toolbar from '$lib/components/Toolbar.svelte';
+	import db from '$lib/data/db';
 	import type { Xact } from '$lib/data/model';
 	import { onMount } from 'svelte';
 
-    let xacts: Xact[] = $state([])
+	let xacts: Xact[] = $state([]);
 
-    onMount(async () => {
-        // load data
-    })
+	onMount(async () => {
+		// load data
+		await loadData();
+	});
+
+	async function loadData() {
+		xacts = await db.xacts.orderBy('date').reverse().toArray();
+	}
 </script>
 
 <Toolbar title="Journal">
@@ -16,12 +22,11 @@
 </Toolbar>
 
 <main>
-    {#if xacts.length == 0}
-    <p>The device journal is empty</p>
-    {:else}
-    {#each xacts as xact}
-        <span>{xact.payee}</span>
-
-    {/each}
-    {/if}
+	{#if xacts.length == 0}
+		<p>The device journal is empty</p>
+	{:else}
+		{#each xacts as xact}
+			<span>{xact.payee}</span>
+		{/each}
+	{/if}
 </main>
