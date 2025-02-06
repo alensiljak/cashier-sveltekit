de947fee045a668a15ad7e82a7c21cf5c207036f
Alen Šiljak <462445+alensiljak@users.noreply.github.com>
2025-02-06 13:48:18 +0100

    fix dialog in journal

--- src/routes/journal/+page.svelte
+++ src/routes/journal/+page.svelte
@@ -7,14 +7,14 @@
 	import db from '$lib/data/db';
 	import { Xact } from '$lib/data/model';
 	import Notifier from '$lib/utils/notifier';
-	import { getModalStore, type ModalSettings } from '@skeletonlabs/skeleton';
+	import { Modal } from '@skeletonlabs/skeleton-svelte';
 	import { FileDownIcon, ImportIcon, PlusIcon, TrashIcon } from 'lucide-svelte';
 	import { onMount } from 'svelte';
 	import { xact } from '$lib/data/mainStore';
 	import type { PageData } from './$types';
 
-	const modalStore = getModalStore();
 	Notifier.init();
+	let isDeleteAllConfirmationOpen = $state(false);
 
 	let listContainer: any;
 	let { data }: { data: PageData } = $props();
@@ -28,24 +28,24 @@
 		listContainer.scrollTop = listContainer.scrollHeight;
 	});
 
+	/**
+	 * Close all dialogs.
+	 */
+	function closeModal() {
+		isDeleteAllConfirmationOpen = false;
+	}
+
 	async function onDeleteAllClicked() {
 		// show confirmation dialog
-		const modal: ModalSettings = {
-			type: 'confirm',
-			// Data
-			title: 'Confirm Delete',
-			body: 'Do you want to delete all transactions?',
-			response: async (r: boolean) => {
-				if (r) {
-					// delete all Xacts
-					await db.xacts.clear();
-					Notifier.success('All local transactions deleted.');
-					// Reload data.
-					await invalidateAll();
-				}
-			}
-		};
-		modalStore.trigger(modal);
+		isDeleteAllConfirmationOpen = true;
+	}
+
+	async function onDeleteAllConfirmed() {
+		// delete all Xacts
+		await db.xacts.clear();
+		Notifier.success('All local transactions deleted.');
+		// Reload data.
+		await invalidateAll();
 	}
 
 	async function onExportClick() {
@@ -99,3 +99,29 @@
 
 	<Fab onclick={onFab} Icon={PlusIcon} />
 </article>
+
+<!-- "Delete All" dialog -->
+<Modal
+	bind:open={isDeleteAllConfirmationOpen}
+	triggerBase="hidden"
+	contentBase="card bg-surface-100-900 p-4 space-y-4 shadow-xl max-w-screen-sm"
+	backdropClasses="backdrop-blur-sm"
+>
+	{#snippet trigger()}Open Modal{/snippet}
+	{#snippet content()}
+		<header class="flex justify-between">
+			<h2 class="h4">Confirm Delete</h2>
+		</header>
+		<article>
+			<p class="opacity-60">Do you want to delete all transactions?</p>
+		</article>
+		<footer class="flex justify-end gap-4">
+			<button type="button" class="variant-tonal btn" onclick={closeModal}>Cancel</button>
+			<button
+				type="button"
+				class="btn-primary variant-filled-primary btn text-tertiary-500"
+				onclick={onDeleteAllConfirmed}>OK</button
+			>
+		</footer>
+	{/snippet}
+</Modal>
