d0bb15403b7cef4f87ef2a44bc7b850d8df54bfc
Alen Šiljak <462445+alensiljak@users.noreply.github.com>
2025-03-11 20:48:26 +0100

    removing the skeleton plugin from tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,12 +3,6 @@ import type { Config } from 'tailwindcss';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import { myCustomTheme } from './cashier-theme.ts'
-// import { cashierTheme } from './cashier-theme-v3.ts';
-
-// 1. Import the Skeleton plugin
-import { skeleton } from '@skeletonlabs/tw-plugin';
-// import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin';
-// import * as themes from '@skeletonlabs/skeleton/themes';
 
 const config = {
 	// 2. Opt for dark mode to be handled via the class method
@@ -29,19 +23,6 @@ const config = {
 		// require('@tailwindcss/typography'),
 		typography,
 		forms,
-		// 4. Append the Skeleton plugin (after other plugins)
-		skeleton({
-			// NOTE: each theme included will be added to your CSS bundle
-			themes: { 
-				//preset: ['gold-nouveau'] 
-				custom: [ myCustomTheme ]
-				// custom: [ cashierTheme ]
-			}
-			//[ 
-			// themes.nouveau,
-			//themes.cerberus, themes.rose 
-			//	]
-		})
 	]
 } satisfies Config;
 
