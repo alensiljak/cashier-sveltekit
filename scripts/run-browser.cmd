bfb39b50817b98a12ff0b920999c45d63ea6ee20
Alen Šiljak <462445+alensiljak@users.noreply.github.com>
2026-04-22 21:06:20 +0200

    chore: browser, dependencies, mcp config

--- scripts/run-browser.cmd
+++ scripts/run-browser.cmd
@@ -1,2 +1,3 @@
 :: Run Vivaldi in remote debugging mode
-%LocalAppData%\Vivaldi\Application\vivaldi.exe --remote-debugging-port=9222
+::%LocalAppData%\Vivaldi\Application\vivaldi.exe --remote-debugging-port=9222
+%USERPROFILE%\scoop\apps\vivaldi\current\Application\vivaldi.exe --remote-debugging-port=9222
